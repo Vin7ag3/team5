cd44bd5f75bfa36485ad4aaca0f85caddecdc60f
Vintage <vincepeo@yahoo.com>
2023-10-22 06:32:09 -0400

    features+style

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 Product.init(
-    {
+  {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -35,5 +35,3 @@ Product.init(
 );
 
 module.exports = Product;
-
-
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const Product = require('../models/product');
 const ProductController = require('../controllers/productController');
 
+// multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// display products
 router.get('/', async (req, res) => {
   try {
-// fetch products using the displayProducts method
     ProductController.displayProducts(req, res);
   } catch (error) {
     console.error('Error fetching products:', error);
@@ -13,6 +27,42 @@ router.get('/', async (req, res) => {
 });
 
 // route to display images
-router.get('/products', ProductController.displayProducts);
+router.get('/products', async (req, res) => {
+  try {
+    ProductController.displayProducts(req, res);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// display the product upload form
+router.get('/upload', (req, res) => {
+  res.render('products', {}); 
+});
+
+// handle the form submission
+router.post('/upload', upload.single('input-file-preview'), async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    const imageUrl = req.file.path;
+
+    const newProduct = {
+      name,
+      description,
+      price,
+      imageUrl: '/uploads/' + req.file.filename,
+    };
+
+// new product into the database using sequelize
+    await Product.create(newProduct);
+
+// respond with a success message or err
+    res.status(200).send('Product uploaded successfully');
+  } catch (error) {
+    console.error('Error uploading product:', error);
+    res.status(500).send('Internal server error');
+  }
+});
 
 module.exports = router;
